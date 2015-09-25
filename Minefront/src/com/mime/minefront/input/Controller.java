5f253b17359f92953d67ddff463b09d3c23284a3
lom <diskedge@yandex.ru>
2015-09-25 08:45:41 +0600

    Created new class Controller

---
+++ Minefront/src/com/mime/minefront/input/Controller.java
@@ -0,0 +1,14 @@
+package com.mime.minefront.input;
+
+public class Controller {
+	
+	public double x, z, rotation, xa, za, rotationa;
+	
+	public void tick(boolean forward, boolean back, boolean left, boolean right, boolean turnLeft, boolean turnRight) {
+		double rotationSpeed = 1;
+		double walkSpeed = 1;
+		double xMove = 0;
+		double zMove = 0;
+	}
+	
+}
